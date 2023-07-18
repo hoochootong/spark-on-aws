93216ec94490ea9f1b33a5ce1b71499a00d544c9
krleejaeho <krleejaeho@gmail.com>
2023-07-18 18:16:55 +0900

    add s3 test

--- src/main/java/com/krleejaeho/SparkReadWriteS3.java
+++ src/main/java/com/krleejaeho/SparkReadWriteS3.java
@@ -3,6 +3,7 @@ package com.krleejaeho;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 
 
@@ -17,6 +18,7 @@ public final class SparkReadWriteS3 {
 
     public static void main(String[] args) throws Exception {
         SparkConf sparkConf = new SparkConf().setAppName("com.krleejaeho.JavaSparkPi");
+        // s3 파일을 읽고 쓰려면 s3afilesystem 을 구현체로 지정 해주어야 함
         sparkConf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem");
 
         SparkSession sparkSession = SparkSession.builder()
@@ -34,6 +36,16 @@ public final class SparkReadWriteS3 {
         df.show();
 
         df.printSchema();
+
+        df.write()
+                .mode(SaveMode.ErrorIfExists)
+                .format("parquet")
+                .partitionBy(args[2])
+                .option("path", args[1])
+                .saveAsTable("sample");
+    }
+
+    public static void writeToStorage(String path, String partition) {
 
     }
 }
